735f2e23b3b74291977f912d9c9dcdb28d51c4a5
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2019-12-11 17:20:30 +0800

    # 添加一些本地文件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/'
+import ElementUI from 'element-ui'
+
+import 'mockjs'
+
+Vue.use(ElementUI, {size: 'mini'})
 
 Vue.config.productionTip = false
 
@@ -10,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
