925fb36d4f580b60119265d1690f8683837fd5bf
guo.changxin <guo.changxin@sfit.shfe.com.cn>
2020-01-02 16:46:44 +0800

    # feat: 更新vuex引入文件的方法

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,26 @@ import Vuex from 'vuex'
 
 import createPersistedState from 'vuex-persistedstate'
 
-import user from './storeModules/user'
-import money from './storeModules/money'
-import menu from './storeModules/menu'
+// import user from './storeModules/user'
+// import money from './storeModules/money'
+// import menu from './storeModules/menu'
 
-if (process.env.NODE_ENV === 'development') {
-  Vue.use(Vuex)
-}
+Vue.use(Vuex)
+
+const files = require.context('./storeModules', false, /\.js$/)
+const modules = {}
+
+files.keys().forEach((key) => {
+  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
+})
 
 export default new Vuex.Store({
-  modules: {
-    user,
-    money,
-    menu
-  },
+  // modules: {
+  //   user,
+  //   money,
+  //   menu
+  // },
+  modules,
   plugins: [
     createPersistedState({
       // storage: {
